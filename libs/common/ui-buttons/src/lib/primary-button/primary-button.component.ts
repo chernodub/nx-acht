15bf668598083b686bbeeb071ff5b0e181feb116
Viktor Chernodub <chernodub98@gmail.com>
2020-08-31 11:34:30 +0700

    (fix) remove unused code from ui-buttons

--- libs/common/ui-buttons/src/lib/primary-button/primary-button.component.ts
+++ libs/common/ui-buttons/src/lib/primary-button/primary-button.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
+/** Primary button UI component. */
 @Component({
   selector: 'acht-primary-button',
   templateUrl: './primary-button.component.html',
   styleUrls: ['./primary-button.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PrimaryButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PrimaryButtonComponent {
 }
